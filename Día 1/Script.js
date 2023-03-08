6ac639778a53120e665237cd53fea71ccb50b68b
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-03-08 01:35:48 -0600

    Update Script.js

--- Día 1/Script.js
+++ Día 1/Script.js
@@ -1,49 +1,60 @@
-let hr = min = sec = ms = "0" + 0, startTimer;
+// Inicializamos las variables de tiempo hr, min, sec y ms con el valor "0" + 0.
+let hr = min = sec = ms = "0" + 0;
 
+// Seleccionamos los botones start, stop y reset del documento HTML.
 const startBtn = document.querySelector(".start"),
     stopBtn = document.querySelector(".stop"),
     resetBtn = document.querySelector(".reset");
 
+// Agregamos un evento click a cada botón, que ejecuta las funciones correspondientes.
 startBtn.addEventListener("click", start);
 stopBtn.addEventListener("click", stop);
 resetBtn.addEventListener("click", reset);
 
+// Función que inicia el temporizador.
 function start() {
+    // Agregamos la clase "active" al botón de start y eliminamos la clase "stopActive" del botón de stop.
     startBtn.classList.add("active");
     stopBtn.classList.remove("stopActive");
 
+    // Establecemos el temporizador y actualizamos los valores de ms, sec, min y hr.
     startTimer = setInterval(() => {
-        ms++; 
+        ms++;
         ms = ms < 10 ? "0" + ms : ms;
 
         if(ms == 100) {
             sec++;
             sec = sec < 10 ? "0" + sec : sec;
-            ms = "0" + 0; 
+            ms = "0" + 0;
         }
         if(sec == 60) {
             min++;
             min = min < 10 ? "0" + min : min;
-            sec = "0" + 0; 
+            sec = "0" + 0;
         }
         if(min == 60) {
             hr++;
             hr = hr < 10 ? "0" + hr : hr;
-            min = "0" + 0; 
+            min = "0" + 0;
         }
 
+        // Actualizamos los valores de tiempo en la página HTML.
         putValue();
 
     }, 10);
 }
 
+// Función que detiene el temporizador.
 function stop() {
+    // Eliminamos las clases "active" y "stopActive" de los botones y detenemos el temporizador.
     startBtn.classList.remove("active");
     stopBtn.classList.remove("stopActive");
     clearInterval(startTimer);
 }
 
+// Función que reinicia el temporizador.
 function reset(){
+    // Eliminamos las clases "active" y "stopActive" de los botones, detenemos el temporizador y reiniciamos los valores de tiempo.
     startBtn.classList.remove("active");
     stopBtn.classList.remove("stopActive");
     clearInterval(startTimer);
@@ -51,6 +62,7 @@ function reset(){
     putValue();
 }
 
+// Función que actualiza los valores de tiempo en la página HTML.
 function putValue() {
     document.querySelector('.millisecond').innerHTML = ms;
     document.querySelector('.second').innerHTML = sec;

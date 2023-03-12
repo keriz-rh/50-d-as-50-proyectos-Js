8958caf8083d2989323eedf34771ec653441b26b
Kevin Rivera <114261500+Kerizr@users.noreply.github.com>
2023-03-11 22:14:22 -0600

    Update Script.js

--- Día 3/Script.js
+++ Día 3/Script.js
@@ -1,18 +1,10 @@
-// Selecciona los elementos del DOM necesarios
+// Seleccionar elementos del DOM
+const wrapper = document.querySelector('.wrapper'); // Contenedor principal
+SelectBtn = wrapper.querySelector('.select-btn'); // Botón que muestra/oculta las opciones
+searchInp = wrapper.querySelector('input'); // Campo de búsqueda
+options = wrapper.querySelector('.options'); // Lista de opciones
 
-// Selecciona el elemento con la clase 'wrapper' en el documento HTML y lo asigna a la constante 'wrapper'
-const wrapper = document.querySelector('.wrapper');
-
-// Selecciona el botón con la clase 'select-btn' dentro del elemento con la clase 'wrapper' y lo asigna a la variable 'SelectBtn'
-SelectBtn = wrapper.querySelector('.select-btn');
-
-// Selecciona el input dentro del elemento con la clase 'wrapper' y lo asigna a la variable 'searchInp'
-searchInp = wrapper.querySelector('input');
-
-// Selecciona el elemento con la clase 'options' dentro del elemento con la clase 'wrapper' y lo asigna a la variable 'options'
-options = wrapper.querySelector('.options');
-
-// Array de países
+// Lista de países disponibles
 let countries = ["Estados Unidos", "Canadá", "México", "Brasil", "Argentina", "Chile", "Reino Unido",
                 "Alemania", "Francia", "España", "Italia", "Japón", "China", 
                 "India", "Rusia", "Australia", "Nueva Zelanda", "Sudáfrica", 
@@ -38,70 +30,52 @@ let countries = ["Estados Unidos", "Canadá", "México", "Brasil", "Argentina", 
                 "Seychelles", "Comoras", "Mauricio", "Madagascar", "Lesoto", "Suazilandia", "Botsuana",
                 "Burundi", "Ruanda", "Burkina Faso", "Chad", "Malí", "Níger", "Sahara Occidental", ]; 
 
-// Define una función que agrega los países a la lista de opciones
+// Función para agregar la lista de países a las opciones
 function addCountry(selectedCountry) {
-options.innerHTML = '';
-// Recorre el arreglo de países y para cada país crea un elemento li que se agregará a la lista de opciones
-countries.forEach(country => {
-// Comprueba si el país actual es el seleccionado
-let isSelected = country == selectedCountry ? 'selected' : '';
-// Crea el elemento li y lo agrega a la lista de opciones
-let li = <li onclick = "updateName(this)" class="${isSelected}">${country}</li>;
-options.insertAdjacentHTML("beforeend", li);
-});
+    // Limpiar la lista de opciones
+    options.innerHTML = '';
+    // Iterar sobre la lista de países y crear una opción para cada uno
+    countries.forEach(country => {
+        // Determinar si este país está seleccionado actualmente
+        let isSelected = country == selectedCountry ? 'selected' : '';
+        // Crear una opción para este país y agregarla a la lista
+        let li = `<li onclick = "updateName(this)" class="${isSelected}">${country}</li>`;
+        options.insertAdjacentHTML("beforeend", li);
+    });
 }
-
-// Llama a la función addCountry para agregar los países a la lista de opciones
+// Mostrar la lista de países por defecto al cargar la página
 addCountry();
 
-// Define una función que actualiza el nombre del país seleccionado
+// Función para actualizar el campo de búsqueda y la lista de opciones cuando se selecciona un país
 function updateName(selectedLi){
-// Limpia el valor del input de búsqueda
-searchInp.value = "";
-// Agrega el país seleccionado a la lista de opciones
-addCountry(selectedLi.innerHTML);
-// Elimina la clase 'active' del elemento 'wrapper'
-wrapper.classList.remove('active');
-// Actualiza el nombre del país seleccionado en el botón de selección
-SelectBtn.firstElementChild.innerText = selectedLi.innerText;
+    // Limpiar el campo de búsqueda
+    searchInp.value = "";
+    // Agregar la lista de opciones actualizada con el país seleccionado
+    addCountry(selectedLi.innerHTML);
+    // Ocultar la lista de opciones
+    wrapper.classList.remove('active');
+    // Cambiar el texto del botón para mostrar el país seleccionado
+    SelectBtn.firstElementChild.innerText = selectedLi.innerText;
 }
 
-// Agrega un evento que se activa cuando el usuario escribe en el input de búsqueda
+// Escuchar eventos de teclado en el campo de búsqueda
 searchInp.addEventListener("keyup", () => {
-// Define un arreglo vacío
-let arr = [];
-// Obtiene la palabra buscada en el input y la convierte en minúsculas
-let searchWord = searchInp.value.toLowerCase();
-// Filtra los países que comienzan con la palabra buscada
-arr = countries.filter(data => {
-return data.toLowerCase().startsWith(searchWord);
-// Crea un elemento li para cada país filtrado y lo agrega al arreglo
-}).map(data => {
-// Comprueba si el país actual es el seleccionado
-let isSelected = data == SelectBtn.firstElementChild.innerText ? "selected" : "";
-return <li onclick="updateName(this)" class="${isSelected}">${data}</li>;
-// Convierte el arreglo de elementos li en una cadena de texto
-}).join("");
-// Si el arreglo está vacío, agrega un mensaje de error
-// Este evento se activa cuando se está escribiendo en el campo de búsqueda
-// Filtra los países que coinciden con la palabra buscada
-// Crea un nuevo arreglo 'arr' con los elementos de la lista que coinciden con la búsqueda
-// Si el arreglo 'arr' no está vacío, se actualiza el contenido de la lista con esos elementos
-// Si el arreglo 'arr' está vacío, se muestra un mensaje que indica que no se encontraron países
-searchInp.addEventListener("keyup", () => {
-let arr = [];
-let searchWord = searchInp.value.toLowerCase();
-arr = countries.filter(data => {
-return data.toLowerCase().startsWith(searchWord);
-}).map(data => {
-let isSelected = data == SelectBtn.firstElementChild.innerText ? "selected" : "";
-return <li onclick="updateName(this)" class="${isSelected}">${data}</li>;
-}).join("");
-  
-  
-options.innerHTML = arr ? arr : <p style = 'margin-top: 10 px;'>Oops! Country not foud</p>;
+    // Crear una lista vacía para las opciones que coincidan con la búsqueda
+    let arr = [];
+    // Obtener la palabra de búsqueda actual
+    let searchWord = searchInp.value.toLowerCase();
+    // Filtrar la lista de países para obtener solo los que empiezan con la palabra de búsqueda
+    arr = countries.filter(data => {
+        return data.toLowerCase().startsWith(searchWord);
+    }).map(data => {
+        // Determinar si esta opción está seleccionada actualmente
+        let isSelected = data == SelectBtn.firstElementChild.innerText ? "selected" : "";
+        // Crear una opción para este país y agregarla a la lista
+        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    }).join("");
+    // Actualizar la lista de opciones con las opciones que coincidan con la búsqueda
+    options.innerHTML = arr ? arr : `<p style = 'margin-top: 10 px;'>Oops! Country not foud</p>`;
 });
 
-// Este evento se activa cuando se hace clic en el botón "Seleccionar"
-// Agrega o elimina la clase "active" del contenedor de la lista desplegable
+// Escuchar eventos de clic en el botón que muestra/oculta las opciones
 SelectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
